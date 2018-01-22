b715e09ea3c3dd601222d0153714d89b8e769471
Denys Astapov <cr0we@icloud.com>
2018-01-22 20:05:32 +0200

    Initial Commit

---
+++ SuperBMI v1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperBMI v1.0
+//
+//  Created by Denys Astapov on 22.01.2018.
+//  Copyright © 2018 Denys Astapov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
